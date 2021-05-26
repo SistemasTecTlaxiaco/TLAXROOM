5fd696d7848cade28df8ca06089b4761738376f8
CarlosSD16 <jcsanti16@gmail.com>
2021-05-26 00:42:59 -0500

    subiendologin

--- login.php
+++ login.php
@@ -1,47 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Login_TlaxRoom</title>
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
-	<!--Estilos-->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet"  href="image/icon/style.css">
+<?php session_start();
+
+    if(isset($_SESSION['usuario'])) {
+        header('location: index.php');
+    }
+
+    $error = '';
     
-	<!--Jquery-->
-</head>
-<body>
-<!--contenedor de formularios-->	
-<div class="container-form">
-	<div class="header">
-		<div class="logo-title">
-			<img src="image/logo_tlax_room.png" alt="">
-		</div>
-		<div class="menu">
-			<a href="#login_inquilino.php"><li class="module-login active">Inquilino</li></a>
-			<a href="#login_arrendador.php"><li class="module-logina">Arrendador</li></a>
-		</div>
-	</div>
-	<div id="login_inquilino" >
-			<form action="/" method="post" class="form">
-				<div class="welcome-form"><h1>Bienvenido<h1></div>
-				<div class="user line-input">
-					<label class="lnr lnr-user"></label>
-					<input type="email"required autocomplete="off" placeholder="Nombre de usuario" name="usuario"/>
-				</div>
-				<div class="password line-input">
-					<label class="lnr lnr-lock"></label>
-					<input type="password"required autocomplete="off" placeholder="Contraseña" name="clave"/>
-				</div>
-				<p class="forgot"><a href="#">Olvidaste la contraseña?</a></p>
-					
-				<button type="submit" class="button button-block">Iniciar Sesión</button>
-			</form>
-	</div>
-</div>
-<script src="js/jquery.js"></script>
-<script src="js/script.js"></script>
-<link href="css/style.css<?=date('Y-m-d H:i:s')?>" rel="stylesheet" type="text/css">
-</body>
-</html>
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['clave'];
+        $clave = hash('sha512', $clave);
+        
+        try{
+            $conexion = new PDO('mysql:host=localhost;dbname=tlax', 'root', '');
+            }catch(PDOException $prueba_error){
+                echo "Error: " . $prueba_error->getMessage();
+            }
+        
+        $statement = $conexion->prepare('SELECT * FROM login WHERE usuario = :usuario AND clave = :clave');
+        
+        $statement->execute(array(
+            ':usuario' => $usuario,
+            ':clave' => $clave
+        ));
+            
+        $resultado = $statement->fetch();
+        
+        if ($resultado !== false){
+            $_SESSION['usuario'] = $usuario;
+            header('location: principal.php');
+        }else{
+            $error .= '<i>Este usuario no existe</i>';
+        }
+    }
+    
+require 'frontend/login-vista.php';
+
+
+?>
