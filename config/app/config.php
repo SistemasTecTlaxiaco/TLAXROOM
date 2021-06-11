c493a01cc0567f4bf63235b7ac1bcead17fa3681
EDITH-R <rese-97@hotmail.com>
2021-06-10 22:14:50 -0500

    archivo htacces

---
+++ config/app/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL','https://localhost/tlaxroom/');
+define('HOST', 'localhost:3307');
+define('DB', 'mvc');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+?>
