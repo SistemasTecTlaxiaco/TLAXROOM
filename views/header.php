02c777865f405ef7412867a499fbd4f30379748c
EDITH-R <rese-97@hotmail.com>
2021-06-26 21:53:19 -0500

    agregando enlaces

--- views/header.php
+++ views/header.php
@@ -29,7 +29,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-text mx-3">TLAXROOM</div>
             </a>
 
--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
 <?php
 require_once "../header.php";
+//esta es la vista del administrador
+echo "comienza el contenido del admin";
 require_once "../footer.php";
 ?>
